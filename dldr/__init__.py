716cdaf3f4b2e18ccf0f8ab998324ad55783208b
Simon Støvring <simonstoevring@gmail.com>
2016-01-22 14:07:39 +0100

    Fills in __init__.py

---
+++ dldr/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+Download programs from DR TV.
+"""
+
+__title__ = 'dldr'
+__version__ = '1.0.0'
+__author__ = 'Simon Støvring'
+__license__ = 'MIT License'
+
